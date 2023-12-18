73a98ed876bef36e83c58127c451e286c39bd05c
kamranlatifch <kamranlatif98@gmail.com>
2023-12-18 15:55:28 +0500

    Private Folder

---
+++ routing-demo/src/app/_lib/page.tsx
@@ -0,0 +1,7 @@
+export default function PrivateRoute(){
+    return <h1>This is Private folder page. All private folder will sstart with _[Underscore]You cannot view this in browser
+        -It helps seperating UI logic from routing logic 
+        - organizing internal files in a project etc
+        - %5F url encoded for underscore
+    </h1>
+}
